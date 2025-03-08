38abbee4bce09447a4bc9458f2b307bf1d72fc7c
Hayk-1315 <162503366+Hayk-1315@users.noreply.github.com>
2025-03-08 18:52:35 +0100

    Update Retry.jsx

--- src/components/Retry.jsx
+++ src/components/Retry.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-//visto
+
 function Retry() {
   return (
     <div className='flex items-center justify-center h-screen text-2xl'>I am sorry but your search cannot be found. Please check to see if you typed it in correctly and try again.</div>
